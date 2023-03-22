5163ce53c0aca16a6919660d71ed468f7a132154
James L <lang.n.james@gmail.com>
2023-03-22 09:35:57 -0500

    Added datamodel for tasks into project.

---
+++ main/java/datamodel/Task.java
@@ -0,0 +1,66 @@
+package datamodel;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "tasks")
+public class Task{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "task")
+	private String task;
+	
+	@Column(name = "description")
+	private String description;
+	
+	public Task() {
+	}
+	
+	public Task(Integer id, String task, String description) {
+		this.id = id;
+		this.task = task;
+		this.description = description;
+	}
+	
+	public Task(String task, String description) {
+		this.task = task;
+		this.description = description;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getTask() {
+		return task;
+	}
+	
+	public void setTask(String task) {
+		this.task = task;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	 @Override
+	   public String toString() {
+	      return "Task #" + this.id + " - " + this.task + ": " + this.description;
+	   }
+}
